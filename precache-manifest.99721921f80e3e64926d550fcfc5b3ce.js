7bb6a686a9c65d3cb1227fdc22fd0c61d61f409c
Inna <inna.tereshchenko1@gmail.com>
2020-01-24 23:43:00 +0200

    Updates

---
+++ precache-manifest.99721921f80e3e64926d550fcfc5b3ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d176667f081e63e49264cc55309b3530",
+    "url": "/password_manager/index.html"
+  },
+  {
+    "revision": "dfc69cbd4b13a3e0ed84",
+    "url": "/password_manager/static/css/main.d1e976df.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/password_manager/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "dfc69cbd4b13a3e0ed84",
+    "url": "/password_manager/static/js/main.bee0e254.chunk.js"
+  },
+  {
+    "revision": "2c4d7f5cc6fc7267cc13",
+    "url": "/password_manager/static/js/runtime~main.bfef69bb.js"
+  }
+]);
